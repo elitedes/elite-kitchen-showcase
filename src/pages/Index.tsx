a0e2daa60058454ec54610263c574d4f5acde09d
elitedes <elitedes@users.noreply.github.com>
2026-01-27 13:41:32 +0200

    Add Benefits section, update translations, and separate section titles

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,6 +1,7 @@
 import Layout from '@/components/layout/Layout';
 import HeroSection from '@/components/home/HeroSection';
 import AboutSection from '@/components/home/AboutSection';
+import BenefitsSection from '@/components/home/BenefitsSection';
 import KitchenCategoriesSection from '@/components/home/KitchenCategoriesSection';
 import WhyUsSection from '@/components/home/WhyUsSection';
 import TestimonialsSection from '@/components/home/TestimonialsSection';
@@ -11,6 +12,7 @@ const Index = () => {
     <Layout>
       <HeroSection />
       <AboutSection />
+      <BenefitsSection />
       <KitchenCategoriesSection />
       <WhyUsSection />
       <TestimonialsSection />
